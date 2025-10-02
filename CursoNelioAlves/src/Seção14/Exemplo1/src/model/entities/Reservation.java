9dfde46e6e17a0b2ee1e9e65717cd625d2453f34
Gabs-jg <gabriel19joao@gmail.com>
2025-10-02 02:50:27 -0300

    Exceptions

--- CursoNelioAlves/src/Seção14/Exemplo1/src/model/entities/Reservation.java
+++ CursoNelioAlves/src/Seção14/Exemplo1/src/model/entities/Reservation.java
@@ -1,5 +1,7 @@
 package Seção14.Exemplo1.src.model.entities;
 
+import Seção14.Exemplo1.src.model.exceptions.DomainException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -15,7 +17,10 @@ public class Reservation {
 
     }
 
-    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {
+        if(!checkOut.after(checkIn)) {
+            throw new DomainException("Check-out date must be after check-in date");
+        }
         this.roomNumber = roomNumber;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
@@ -42,7 +47,14 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public void updateDates(Date checkIn, Date checkOut) {
+    public void updateDates(Date checkIn, Date checkOut) throws DomainException {
+        Date now = new Date();
+        if(checkIn.before(now) || checkOut.before(now)) {
+            throw new DomainException("Reservation dates for updates must be future");
+        }
+        if(!checkOut.after(checkIn)) {
+            throw new DomainException("Check-out date must be after check-in date");
+        }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
     }
